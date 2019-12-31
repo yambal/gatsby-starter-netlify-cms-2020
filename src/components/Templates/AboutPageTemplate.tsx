6c5e3c3853d3924e3b399e2254323287b0601160
June YAMAMOTO <june@yambal.net>
2019-12-31 11:38:01 +0900

    Refact About

---
+++ src/components/Templates/AboutPageTemplate.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Content from '../Content'
+
+interface iAboutPageTemplate {
+  title: string
+  content: any
+  contentComponent: any
+}
+
+export const AboutPageTemplate:React.FC<iAboutPageTemplate> = (props) => {
+  const PageContent = props.contentComponent || Content
+
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {props.title}
+              </h2>
+              <PageContent className="content" content={props.content} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutPageTemplate
--- src/containers/aboutContainer.tsx
+++ src/containers/aboutContainer.tsx
@@ -1,38 +1,17 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import { HTMLContent } from '../components/Content'
+import AboutPageTemplate from '../components/Templates/AboutPageTemplate'
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
-
-  return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+interface iAboutContainer {
+  data: {
+    markdownRemark: any
+  }
 }
 
-AboutPageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func,
-}
-
-const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+const AboutPage:React.FC<iAboutContainer> = (props) => {
+  const { markdownRemark: post } = props.data
 
   return (
     <Layout>
@@ -43,10 +22,6 @@ const AboutPage = ({ data }) => {
       />
     </Layout>
   )
-}
-
-AboutPage.propTypes = {
-  data: PropTypes.object.isRequired,
 }
 
 export default AboutPage
