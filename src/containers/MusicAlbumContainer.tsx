9287506c27967fc0a3ab44187884f84573f8723f
June YAMAMOTO <june@yambal.net>
2020-01-04 01:08:18 +0900

    Dev

--- src/containers/MusicAlbumContainer.tsx
+++ src/containers/MusicAlbumContainer.tsx
@@ -4,50 +4,25 @@ import Layout from '../components/Layout'
 import IndexPagePage from '../components/pages/IndexPagePage'
 import Container from '../components/Container'
 
-/*
 export const pageQuery = graphql`
-  query IndexContainer {
-    markdownRemark(frontmatter: { templateKey: { eq: "Index" } }) {
+  query MusicAlbumByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
       frontmatter {
-        title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        subheading
-        mainpitch {
+        songs {
+          file
           title
-          description
         }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
+        title
       }
     }
   }
 `
-*/
 
 const MusicAlbumContainer:React.FC = (props) => {
   return (
     <Layout>
-        <pre>{JSON.stringify(props, null, 2)}</pre>
+      <pre>{JSON.stringify(props, null, 2)}</pre>
     </Layout>
   )
 }
