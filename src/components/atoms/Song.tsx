ce428f35bc234567dbf23bc43c72239bce002478
June YAMAMOTO <june@yambal.net>
2020-01-05 02:05:22 +0900

    Audio

--- src/components/atoms/Song.tsx
+++ src/components/atoms/Song.tsx
@@ -6,11 +6,16 @@ interface iSong {
   songFile: iReleaseSongFile | string
 }
 
+
+
 const Song:React.FC<iSong> = (props) => {
   return (
     <div>
       <h2>{props.songTitle}</h2>
-      {typeof props.songFile === 'object' ? <pre>{JSON.stringify(props.songFile, null, 2)}</pre> : <p>Dummy</p>}
+      {typeof props.songFile === 'object' ? <pre>
+        {JSON.stringify(props.songFile, null, 2)}
+        <audio src={props.songFile.publicURL} controls/>
+      </pre> : <p>Dummy</p>}
     </div>
   )
 }
