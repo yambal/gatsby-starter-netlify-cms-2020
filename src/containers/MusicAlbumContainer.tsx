d71847662ceeb92c5c9c5f44beb73a35fd52cb1c
June YAMAMOTO <june@yambal.net>
2020-01-06 00:11:51 +0900

    description

--- src/containers/MusicAlbumContainer.tsx
+++ src/containers/MusicAlbumContainer.tsx
@@ -5,6 +5,8 @@ import IndexPagePage from '../components/pages/IndexPagePage'
 import Container from '../components/Container'
 import MusicAlbum from '../components/pages/MusicAlbum'
 import { iSong } from '../components/atoms/Song'
+import Img from "gatsby-image"
+import Column from '../components/atoms/Column'
 
 export const pageQuery = graphql`
   query MusicAlbumByID($id: String!) {
@@ -12,21 +14,19 @@ export const pageQuery = graphql`
       id
       frontmatter {
         title
+        description
+        featuredimage {
+          childImageSharp {
+            fluid(maxWidth: 500, quality: 100) {
+              ...GatsbyImageSharpFluid
+              presentationWidth
+            }
+          }
+        }
       }
     }
   }
 `
-
-/*
-export interface iReleaseSongFile {
-  publicURL: string
-  prettySize: string
-  internal: {
-    mediaType:string
-    contentDigest: string
-  }
-}
-*/
 
 interface iMusicAlbumContainerPageResourcesJsonPageContext {
   songs: iSong[]
@@ -46,6 +46,8 @@ interface iMusicAlbumContainer {
       id: string
       frontmatter: {
         title: string
+        description: string
+        featuredimage: any
       }
     }
   }
@@ -54,15 +56,26 @@ interface iMusicAlbumContainer {
 
 const MusicAlbumContainer:React.FC<iMusicAlbumContainer> = (props) => {
   const { data:{ markdownRemark:{ frontmatter: album }}} = props
-  const { title: albumTitle } = album
+  const { title: albumTitle, description } = album
 
   const { pageResources } = props
+
+  const f = props.data.markdownRemark.frontmatter.featuredimage.childImageSharp.fluid
 
   if(pageResources && pageResources.json){
     const { json:{pageContext:{songs}}} = pageResources
     return (
       <Layout>
-        {songs && <MusicAlbum albumTitle={albumTitle} songs={songs} />}
+        <Container>
+          <Column>
+            <Img fluid={f} style={{width:'100%'}}/>
+            <p>{description}</p>
+            {songs && <MusicAlbum
+              albumTitle={albumTitle}
+              songs={songs}
+            />}
+          </Column>
+        </Container>
       </Layout>
       )
   }
