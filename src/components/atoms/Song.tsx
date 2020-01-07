205cf16f41a3421051268a566d330791daaa1c08
June YAMAMOTO <june@yambal.net>
2020-01-08 00:17:20 +0900

    Player

--- src/components/atoms/Song.tsx
+++ src/components/atoms/Song.tsx
@@ -15,14 +15,28 @@ export interface iSong {
 
 interface iSongCompo {
   song: iSong
+  track?: number
+  onPlayHandler?: (track: number) => void
+  onPauseHandler?: (track: number) => void
+  onEndHandler?: (track: number) => void
 }
 
-const Song:React.FC<iSongCompo> = (props) => {
+/*
+const SongBase: React.RefForwardingComponent<any, iSongCompo> = (props, ref) => {
   const [isPlaying, setIsPlaying] = React.useState<boolean>(false)
   const [isCanplaythrough, setIsCanplaythrough] = React.useState<boolean>(false)
   const [currentTime, setCurrentTime] = React.useState<number>(0)
   const [duration, setDuration] = React.useState<number>(0)
   const audioRef = React.useRef(null)
+
+  // - - - - - - - - - - - - - - - - - - - - - -
+    const test = () => {
+      console.log(29)
+    };
+    React.useImperativeHandle(ref, () => ({
+      test
+    }));
+  // - - - - - - - - - - - - - - - - - - - - - -
 
   const handlePlay = React.useCallback(
     () => {
@@ -67,7 +81,7 @@ const Song:React.FC<iSongCompo> = (props) => {
     []
   )
 
-  /** 再生可能になった */
+  // 再生可能になった
   const onCanPlay = React.useCallback(
     (e:React.StyleHTMLAttributes<HTMLAudioElement>) => {
       setDuration(audioRef.current.duration)
@@ -75,9 +89,7 @@ const Song:React.FC<iSongCompo> = (props) => {
     []
   )
 
-  /**
-   * 途切れ無しで再生できそう
-   */
+  // 途切れ無しで再生できそう
   const onCanPlayThrough = React.useCallback(
     (e:React.StyleHTMLAttributes<HTMLAudioElement>) => {
       setIsCanplaythrough(true)
@@ -85,9 +97,7 @@ const Song:React.FC<iSongCompo> = (props) => {
     []
   )
 
-  /**
-   * 一時停止されたとき
-   **/
+  // 一時停止されたとき
   const onPause = React.useCallback(
     (e:React.StyleHTMLAttributes<HTMLAudioElement>) => {
       setIsPlaying(false)
@@ -113,5 +123,91 @@ const Song:React.FC<iSongCompo> = (props) => {
     </div>
   )
 }
+*/
+
+export interface iSongRef {
+  track: number
+  pray: () => void 
+  pause: () => void 
+}
+
+const SongBase: React.RefForwardingComponent<any, iSongCompo> = (props, ref) => {
+  const audioRef = React.useRef(null)
+  const [isPlaying, setIsPlaying] = React.useState<boolean>(false)
+  const [currentTime, setCurrentTime] = React.useState<number>(0)
+  const [duration, setDuration] = React.useState<number>(0)
+
+  // 再生
+  const handlePlay = React.useCallback(
+    () => {
+      const audio:HTMLAudioElement = audioRef.current
+      audioRef.current.currentTime = 0;
+      audio.play()
+      .then(() => {
+        setIsPlaying(true)
+        props.onPlayHandler && props.onPlayHandler(props.track)
+      })
+    },
+    []
+  )
+
+  // 停止
+  const handlePause = React.useCallback(
+    () => {
+      const audio:HTMLAudioElement = audioRef.current
+      audio.pause()
+      setIsPlaying(false)
+      props.onPauseHandler && props.onPauseHandler(props.track)
+    },
+    []
+  )
+
+  const onEnd = React.useCallback(
+    () => {
+      audioRef.current.pause()
+      props.onEndHandler && props.onEndHandler(props.track)
+      setIsPlaying(false)
+    },
+    []
+  )
+
+  const onCanPlay = React.useCallback(
+    (e:React.StyleHTMLAttributes<HTMLAudioElement>) => {
+      setDuration(audioRef.current.duration)
+    },
+    []
+  )
+
+  const onTimeUpdate = React.useCallback(
+    (e:React.StyleHTMLAttributes<HTMLAudioElement>) => {
+      setCurrentTime(audioRef.current.currentTime)
+    },
+    []
+  )
+
+  // ref が使われた時に親コンポーネントに渡されるインスタンス値をカスタマイズする
+  React.useImperativeHandle(ref, () => ({
+    track: props.track,
+    pray: handlePlay,
+    pause: handlePause
+  }));
+
+  return (
+    <div>
+      {props.track}.{props.song.title}
+      {!isPlaying && <button onClick={handlePlay}>play</button>}
+      {isPlaying && <button onClick={handlePause}>pause</button>}
+      {currentTime} / {duration} {Math.round(currentTime / duration * 100)}%
+      <audio
+        src={props.song.file.publicURL}
+        ref={audioRef}
+        onEnded={onEnd}
+        onTimeUpdate={onTimeUpdate}
+        onCanPlay={onCanPlay}
+      />
+    </div>
+  )
+} 
+const Song = React.forwardRef(SongBase);
 
 export default Song
--- src/components/pages/MusicAlbum.tsx
+++ src/components/pages/MusicAlbum.tsx
@@ -7,32 +7,83 @@ import Container from '../Container';
 import Column from '../atoms/Column';
 import { Helmet } from 'react-helmet';
 import PageTitle from '../atoms/PageTitle';
-import Song, { iSong } from '../atoms/Song'
+import Song, { iSong, iSongRef } from '../atoms/Song'
+import { number } from 'prop-types';
 
 interface iMusicAlbum {
   albumTitle: string
   songs: iSong[]
 }
 
-export const MusicAlbum:React.FC<iMusicAlbum> = (props) => {
-  return (
-    <Container>
-      <h1>{ props.albumTitle }</h1>
-      {props.songs.map((song, index) => {
-        return(
-          <Song song={song} key={`song-${index}`}/>
-        )
-      })}
-    </Container>
-  )
+interface iMusicAlbumState {
+  refs: iSongRef[]
+}
+
+class MusicAlbum extends React.Component<iMusicAlbum, iMusicAlbumState> {
+  constructor(props: iMusicAlbum) {
+    super(props);
+    this.state = {
+      refs: []
+    }
+  }
+  
+
+  pushRef = (newRef: iSongRef) => {
+    if(newRef) {
+      const checkRef = this.state.refs.filter((ref) => {
+        return ref.track === newRef.track
+      })
+      checkRef.length === 0 && this.state.refs.push(newRef)
+    }
+  }
+
+  onPlay = (track: number) => {
+    this.state.refs.map((ref) => {
+      if(ref.track !== track) {
+        ref.pause()
+      }
+    })
+  }
+
+  onPause = (track: number) => {
+    console.log('pause', track)
+  }
+
+  onEnd = (track: number) => {
+    const nextRef = this.state.refs.filter(
+      (ref) => {
+        return ref.track === track + 1
+      }
+    )
+    if (nextRef.length !== 0) {
+      this.state.refs[nextRef[0].track].pray()
+      return
+    }
+    this.state.refs[0].pray()
+  }
+
+  render() {
+    return (
+      <Container>
+        <h1>{ this.props.albumTitle }</h1>
+        {this.props.songs.map((song, index) => {
+          return(
+            <Song
+              song={song}
+              key={`song-${index}`}
+              track={index}
+              ref={this.pushRef}
+              onPlayHandler={(track: number) => {
+                this.onPlay(track)
+              }}
+              onPauseHandler={this.onPause}
+              onEndHandler={this.onEnd}
+            />
+          )
+        })}
+      </Container>
+    )
+  }
 }
 
 export default MusicAlbum
-
-/*
-{props.songs.map((song, index) => {
-        return(
-          <Song song={song} key={`song-${index}`}/>
-        )
-      })}
-*/
