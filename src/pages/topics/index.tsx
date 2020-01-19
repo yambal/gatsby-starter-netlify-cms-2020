a0f80d034b54b42d5330e2abb9a0b42400753653
June YAMAMOTO <june@yambal.net>
2020-01-19 14:03:10 +0900

    firstcommit

---
+++ src/pages/topics/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+import Container from '../../components/Container'
+import { Router, RouteComponentProps } from "@reach/router"
+import { Link } from 'gatsby'
+import StudyRouterPage from '../../components/morequles/StudyRouterPage'
+import styled from 'styled-components'
+
+interface iTopicsIndex {
+
+}
+
+const StudyIndex: React.FC<iTopicsIndex> = (props) => {
+    return(
+      <Layout dark>
+        <Container>
+            <pre>{JSON.stringify(props, null, 2)}</pre>
+        </Container>
+      </Layout>
+    )
+  }
