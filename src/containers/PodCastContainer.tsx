b7c7eb1f82da2138f9eb494e40e2e5dcaa7adffc
June YAMAMOTO <june@yambal.net>
2020-02-08 12:32:30 +0900

    Header Link

--- src/containers/PodCastContainer.tsx
+++ src/containers/PodCastContainer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import { graphql } from 'gatsby'
+import { Helmet } from 'react-helmet'
 
 export const pageQuery = graphql`
   query PodCastContainer($id: String!) {
@@ -33,10 +34,13 @@ const PodCastContainer:React.FC<iPodCastContainer> = (props) => {
 
   const { data: { markdownRemark: { mp3 } }} = props
 
+  
   return (
     <Layout>
+      <Helmet>
+        <link rel="alternate" type="application/rss+xml" href="/podcast.rss" title="WWW.YAMBAL.NET" />
+      </Helmet>
       <a href={mp3.url}>MP3</a>
-      <pre>{JSON.stringify(props, null, 2)}</pre>
     </Layout>
   )
 }
