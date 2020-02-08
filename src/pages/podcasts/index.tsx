3297b25118d92d7be51f83a2614e6d556fefa9a9
June YAMAMOTO <june@yambal.net>
2020-02-08 12:42:55 +0900

    WWW.YAMBAL.NET Podcast

--- src/pages/podcasts/index.tsx
+++ src/pages/podcasts/index.tsx
@@ -4,10 +4,14 @@ import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
 import Container from '../../components/Container'
 import Column from '../../components/atoms/Column'
+import { Helmet } from 'react-helmet'
 
 const BlogIndexPage:React.FC = () => {
   return (
     <Layout>
+      <Helmet>
+        <link rel="alternate" type="application/rss+xml" href="/podcast.rss" title="WWW.YAMBAL.NET Podcast" />
+      </Helmet>
       <Container>
         <h1>PODCASTS</h1>
       </Container>
