417f307931cd3cd1ae6cace36d912fd67fc8660d
June YAMAMOTO <june@yambal.net>
2020-02-08 22:36:52 +0900

    Channel

--- src/pages/podcasts/index.tsx
+++ src/pages/podcasts/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
@@ -6,7 +7,7 @@ import Container from '../../components/Container'
 import Column from '../../components/atoms/Column'
 import { Helmet } from 'react-helmet'
 
-const BlogIndexPage:React.FC = () => {
+const BlogIndexPage:React.FC = props => {
   return (
     <Layout>
       <Helmet>
@@ -14,9 +15,35 @@ const BlogIndexPage:React.FC = () => {
       </Helmet>
       <Container>
         <h1>PODCASTS</h1>
+        <pre>{JSON.stringify(props)}</pre>
       </Container>
     </Layout>
   )
 }
+
+export const pageQuery = graphql`
+  query PodcastIndexQuery {
+    allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "PodCast"}}}, limit: 10) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            description
+            date
+            channel
+          }
+          mp3 {
+            absoluteUrl
+            url
+            path
+          }
+        }
+      }
+    }
+  }
+`
 
 export default BlogIndexPage
