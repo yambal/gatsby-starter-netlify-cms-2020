3d70587860508cd970ba213f1958511842dcb6f7
June YAMAMOTO <june@yambal.net>
2020-03-18 23:28:10 +0900

    maxLength

--- src/components/templates/PodcastPreview.tsx
+++ src/components/templates/PodcastPreview.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Content from '../Content'
+import { ssmlMaxLength } from 'gatsby-plugin-speach-podcast'
 
 const PodcastPreview: React.FC<any> = props => {
-    const PostContent = props.contentComponent || Content
-    const { entry, widgetFor } = props
+  const PostContent = props.contentComponent || Content
+  const { entry, widgetFor } = props
 
-    const content = widgetFor('body')
+  const content = widgetFor('body')
 
-    return (
-        <PostContent content={content} />
-    )
+  return (
+    <React.Fragment>
+      <div>{ssmlMaxLength(content)}</div>
+      <PostContent content={content} />
+    </React.Fragment>
+  )
 
 }
 
