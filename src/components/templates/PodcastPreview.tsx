5298333aef34407a43f2ec399d7a0095673223c5
June YAMAMOTO <june@yambal.net>
2020-03-18 21:53:47 +0900

    Test

--- src/components/templates/PodcastPreview.tsx
+++ src/components/templates/PodcastPreview.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import Content from '../Content'
 
-const PodcastPreview = (props) => (
-<pre>{JSON.stringify(props, null, 2)}</pre>
-)
+const PodcastPreview: React.FC<any> = props => {
+    const PostContent = props.contentComponent || Content
+    const { entry, widgetFor } = props
+
+    const content = widgetFor('body')
+
+    return (
+        <PostContent content={content} />
+    )
+
+}
 
 export default PodcastPreview
